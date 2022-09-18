dbf3a9ecfaec58b7f32ad6d3c840993c0115d2e3
Emre KARAMAN <matizdrum@gmail.com>
2022-09-18 11:18:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-
+import Main from './components/main';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
-      <section class="menu">
-        
-        <div class="title">
-          <h2>Asian Kitchen's Menu</h2>
-        </div>
-        
-        <div class="btn-container"></div>
-       
-        <div class="section-center row">
-                         
-        </div>
-      </section>
-    </div>
+      <Main />
     </div>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+function Main() {
+    return (      
+        <div class="container">
+        <section class="menu">
+          
+          <div class="title">
+            <h2>Asian Kitchen's Menu</h2>
+          </div>
+          
+          <div class="btn-container"></div>
+         
+          <div class="section-center row">
+                           
+          </div>
+        </section>
+      </div>     
+    );
+  }
+
+  export default Main
